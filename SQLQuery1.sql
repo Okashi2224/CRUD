daff1e91a7f604f327444fa00a12ce2f25d7efc8
Okashi2224 <n.bautista.547563@umindanao.edu.ph>
2025-11-13 21:15:58 +0800

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+CREATE TABLE tbl_inventory_Bautista (
+    ItemID INT IDENTITY(1,1) PRIMARY KEY,
+    ItemName VARCHAR(15), 
+    Description VARCHAR(50), 
+    Quantity INT,
+    Price DECIMAL(10,2),
+    DateAdded DATETIME DEFAULT GETDATE()
+);
